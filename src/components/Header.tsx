22e04457335ca763fbc739ed525cfe720a0aa884
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-03-14 05:58:32 +0100

    edited Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,9 +14,6 @@ const Header = () => {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="scores">High Scores</Link>
-          </li>
         </ul>
 
         {showTimer ? <Timer /> : null}
