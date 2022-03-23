9aac035faf1b2697be2eda6fc1ed7008753834f9
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-03-23 07:52:32 +0100

    build

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { QuizContext } from "../context/context";
 import styles from "../styles/scss/App.module.scss";
 
